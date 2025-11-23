42969c0499bcc1a63c7b9e4984edefd02b635631
Arseniy <arsenijromanov322@gmail.com>
2025-11-24 02:16:32 +0300

    implemented Generator service add protos for WriteService and ReadService

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -1,6 +1,20 @@
+using Generator;
+using UrlShortener.Shared.Infrastructure;
+using SharpJuice.Essentials;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+builder.Services.AddSingleton<IClock, SystemClock>();
+
+var instanceId = builder.Configuration.GetValue<ushort>("Snowflake:InstanceId");
+builder.Services.AddSingleton(new SnowFlakeGeneratorConfig(instanceId));
+builder.Services.AddSingleton<IGenerator, SnowFlakeGenerator>();
+builder.Services.AddSingleton<ShortCodeGeneratorGrpcService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGrpcService<ShortCodeGeneratorGrpcService>();
+app.MapGet("/", () => "Short code generator gRPC service. Use a gRPC client.");
 
 app.Run();
